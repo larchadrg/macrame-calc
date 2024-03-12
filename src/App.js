8b269d615c1a76a5f43cf5e1cb5caee2d1288f56
larchadrg <lara.combina11@gmail.com>
2024-03-12 10:38:54 -0300

    added spanish support

--- src/App.js
+++ src/App.js
@@ -1,82 +1,36 @@
-import './App.css';
-import {showResult} from './cordCalculations.js';
+import Nav from './Nav';
+import WelcomeSection from './WelcomeSection';
+import CordCalculatorSection from './CordCalculatorSection.js';
+import React, { useEffect, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import Footer	from './Footer';
 
 function App() {
+  const { i18n } = useTranslation();
+    const [language, setLanguage] = useState(i18n.language);
+
+    const changeLanguage = (lng) => {
+        i18n.changeLanguage(lng);
+        setLanguage(lng);
+        localStorage.setItem('i18nextLng', lng);
+    };
+
+    useEffect(() => {
+      const lng = localStorage.getItem('i18nextLng');
+      if (lng) {
+          i18n.changeLanguage(lng);
+      }
+      }, [language, i18n]);
+  
   return (
     <div className="App">
-      <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
-        <a className="navbar-brand" href="#">
-            <img src="favicon.png" alt="Logo" 
-            width="30" height="30" 
-            className="d-inline-block align-text-top"></img>
-            Macrame Designer</a>
-    </nav>
+    <Nav changeLanguage={changeLanguage}/>
     <div className="container">
-        <div className="container text-center">
-            <div className="row">
-                <div className="col text-center d-flex flex-column justify-content-center">
-                    <h1>Welcome to Macrame Designer!</h1>
-                    <p>This is a simple tool to help you calculate the length of cord you need for your macrame project. Just enter the dimensions of your project and we'll do the rest!</p>
-                    <form action="#cord-calculator">
-                        <input type="submit" value="Get Started" className="btn btn-primary"></input>
-                    </form>
-                </div>
-                <div className="col-sm">
-                    <img src="spider-logo-beta.png" className="img-fluid"></img>
-                </div>
-            </div>
-        </div>
-        <hr></hr>
-        <section id="cord-calculator">
-            <h2>Cord Length Calculator</h2>
-            <p>Not sure how much cord you need for your macrame project? No problem! We can help you with that. Just enter the dimensions of your project and we'll calculate the length of cord you need.</p>
-            <h3>Alpha Pattern</h3>
-            <p>Note: take into account that this calculation is only for the base cords, 
-                and not for the cords used to make the knots.
-            </p>
-            <p>Enter below the width and height of the bracelet you want to make:</p>
-            <form onSubmit={(e) => showResult(e)}>
-                <div className="mb-3">
-                    <label for="bracelet-width" className="form-label">Width:</label>
-                    <input type="number" id="bracelet-width" name="width" className="form-control" min="1" required></input>
-                </div>
-                <div className="mb-3">
-                    <label for="bracelet-height" className="form-label">Height:</label>
-                    <input type="number" id="bracelet-height" name="height" className="form-control" min="1" required></input>
-                </div>
-                
-                <label for="cm">Show length in: </label>
-                <div className="mb-3">
-                    <div className="form-check form-check-inline">
-                        <input className="form-check-input" type="radio" id="in" name="unit" value="in" required></input>
-                        <label className="form-check-label" for="in">Inches</label>
-                    </div>
-                    <div className="form-check form-check-inline">
-                        <input className="form-check-input" type="radio" id="cm" name="unit" value="cm" required></input>
-                        <label className="form-check-label" for="cm">Centimeters</label>
-                    </div>
-                </div>
-                <label for="begginning">Where are you starting your design from?</label>
-                <div className="mb-3">
-                    <div className="form-check form-check-inline">
-                        <input className="form-check-input" type="radio" id="begginning" name="start" value="begginning" required></input>
-                        <label className="form-check-label" for="begginning">The beginning</label>
-                    </div>
-                    <div className="form-check form-check-inline">
-                        <input className="form-check-input" type="radio" id="middle" name="start" value="middle" required></input>
-                        <label className="form-check-label" for="middle">The middle</label>
-                    </div>
-                </div>
-                <input type="submit" value="Calculate" className="btn btn-primary"></input>
-            </form>
-        </section>
-        <section id="result-section">
-            <div id="result"></div>
-        </section>
+      <WelcomeSection/>
+      <hr></hr>
+      <CordCalculatorSection/>
     </div>
-    <footer className="d-flex flex-wrap justify-content-center align-items-center py-3 mt-3 bg-secondary text-light ">
-        <p className="m-0">Made in March 2024, by Lara Combina. Icons by <a href="https://icons8.com/" className="text-light">icons8.</a></p>
-    </footer>    
+    <Footer/>  
     </div>
   );
 }
--- src/cordCalculations.js
+++ src/cordCalculations.js
@@ -1,47 +1,48 @@
+import { useTranslation } from 'react-i18next';
+
 const WAX_CORD_CM_PER_LINE = 0.4;
 const EXTRA_CORD_PER_SIDE_CM = 15;
 
-export function calculateCordLength(){
-    let height = document.getElementById('bracelet-height').value;
-    let unit = document.querySelector('input[name="unit"]:checked').value;
-    let start = document.querySelector('input[name="start"]:checked').value;
+/**
+ * Calculates the length of cord needed for making a bracelet.
+ * @param {number} height - Height of the bracelet.
+ * @param {string} unit - Unit of measurement for height ("cm" or "in").
+ * @param {string} start - Starting point of the bracelet ("beginning" or "middle").
+ * @returns {string} - Length of cord needed in the specified unit.
+ */
+export function calculateCordLength(height, unit, start) {
     let result = height * WAX_CORD_CM_PER_LINE;
-    if (start == "begginning") {
-        result += EXTRA_CORD_PER_SIDE_CM
-        result = result * 2; 
-    } else {
+    if (start === "beginning") {
         result += EXTRA_CORD_PER_SIDE_CM * 2;
+    } else {
+        result += EXTRA_CORD_PER_SIDE_CM;
     }
-    if (unit == "in") {
-        result = result / 2.54;
+    if (unit === "in") {
+        result /= 2.54;
     }
-    console.log(result);
-    return result.toFixed(1); 
+    return result.toFixed(1);
 }
 
-export function showResult(e){
-    e.preventDefault();
-    let unit = document.querySelector('input[name="unit"]:checked').value;
-    let cordLength = calculateCordLength();
-    let height = document.getElementById('bracelet-height').value;
-    let width = document.getElementById('bracelet-width').value;
-    let start = document.querySelector('input[name="start"]:checked').value;
-    let cordAmount = start == "middle" ? width : Math.ceil(width/2);
-
-    //create new elements
-    let hr = document.createElement('hr');
-    let h2 = document.createElement('h2');
-    h2.innerHTML = 'Result';
-    let p = document.createElement('p');
-    p.innerHTML = `For a bracelet of height ${height} and width ${width},\
-    starting from the ${start},\ 
-    you will need to cut ${cordAmount} base cords of ${cordLength}${unit}. each.`;
-
-    //append to the DOM
-    let resultDiv = document.getElementById('result');
-    resultDiv.innerHTML = '';
-    resultDiv.appendChild(document.createElement('hr'));
-    resultDiv.appendChild(h2);
-    resultDiv.appendChild(p);    
-    window.scrollTo(0, document.getElementById('result').offsetTop)
+/**
+ * Displays a message indicating the amount of cord needed for making a bracelet.
+ * @param {object} props - Props containing height, unit, start, and width.
+ * @returns {JSX.Element} - JSX element displaying the message.
+ */
+export function CordLengthMessage({ height, unit, start, width }) {
+    const { t } = useTranslation("global");
+    let cordLength = calculateCordLength(height, unit, start);
+    let cordAmount = start === "middle" ? width : Math.ceil(width / 2);
+    unit = unit === "in" ? t("CalcForm.form.in") : t("CalcForm.form.cm");
+    start = start === "beginning" ? t("CalcForm.form.start1") : t("CalcForm.form.start2");
+    const translatedResultString = t("CalcForm.result.p", { height:height, width:width, start:start, cordAmount:cordAmount, cordLength:cordLength, unit:unit });
+    console.log(translatedResultString);
+    return (
+        <section id="result-section">
+            <hr />
+            <h2>{t("CalcForm.result.h2")}</h2>
+            <div id="result">
+                <p> {translatedResultString} </p>
+            </div>
+        </section>
+    );
 }
